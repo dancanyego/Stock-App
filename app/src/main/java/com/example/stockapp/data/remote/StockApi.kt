dcf9d1771356ff49f10672cc839fe53cf1b189b9
dancanyego <56153056+dancanyego@users.noreply.github.com>
2022-11-01 15:14:13 +0300

    Retrofit

--- app/src/main/java/com/example/stockapp/data/remote/StockApi.kt
+++ app/src/main/java/com/example/stockapp/data/remote/StockApi.kt
@@ -1,13 +1,16 @@
 package com.example.stockapp.data.remote
 
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface StockApi {
+    @GET("query?functions=LISTING_STATUS")
     suspend fun getListings(
         @Query("apiKey") apikey: String
     )
 
     companion object {
         const val API_KEY = "E3H3DN1PAWP2TD5C"
+        const val   BASE_URL = "https://www.alphavantage.co/"
     }
 }
