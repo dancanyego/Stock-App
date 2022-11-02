283cb1c97c5730d0b24f18ffef5fb26f29f113ac
dancanyego <56153056+dancanyego@users.noreply.github.com>
2022-11-03 01:58:44 +0300

    apis and db setting

--- app/src/main/java/com/example/stockapp/data/local/StockDao.kt
+++ app/src/main/java/com/example/stockapp/data/local/StockDao.kt
@@ -1,11 +1,28 @@
 package com.example.stockapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 
 interface StockDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCompanyListings(
-        comcompanyListingEntities: List<>
+        comcompanyListingEntities: List<CompanyListingEntity>
     )
+    @Query("DELETE FROM companylistingentity")
+    suspend fun clearCompanyListings()
+
+    @Query(
+        """
+            SELECT *
+            FROM companylistingentity
+            WHERE LOWER(name) LIKE '%' || LOWER(:query) || '%' OR 
+                 UPPER(:query) == symbol
+        """
+    )
+
+    suspend fun searchCompanyListing(query: String): List<CompanyListingEntity>
 }
