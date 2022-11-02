f3aaeefefe9ab0a8f21fed16a5393c66c6508054
dancanyego <56153056+dancanyego@users.noreply.github.com>
2022-11-03 02:06:23 +0300

    apis and db setting finishes

---
+++ app/src/main/java/com/example/stockapp/data/local/StockDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.stockapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [CompanyListingEntity::class],
+    version = 1
+)
+
+abstract class StockDatabase: RoomDatabase() {
+    abstract val dao: StockDao
+}
